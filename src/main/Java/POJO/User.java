9d09b00471e02a355f041486b626d57326aa4b34
战在月光下 <1525378562@qq.com>
2020-07-22 01:23:05 +0800

    Add some jars

--- src/main/Java/POJO/User.java
+++ src/main/Java/POJO/User.java
@@ -1,50 +1,18 @@
 package POJO;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User
+{
     private String username;
     private String mm;
     private int permission;
     private int primary_key;
     private int pass;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMm() {
-        return mm;
-    }
-
-    public void setMm(String mm) {
-        this.mm = mm;
-    }
-
-    public int getPermission() {
-        return permission;
-    }
-
-    public void setPermission(int permission) {
-        this.permission = permission;
-    }
-
-    public int getPrimary_key() {
-        return primary_key;
-    }
-
-    public void setPrimary_key(int primary_key) {
-        this.primary_key = primary_key;
-    }
-
-    public int getPass() {
-        return pass;
-    }
-
-    public void setPass(int pass) {
-        this.pass = pass;
-    }
 }
